487f2d63f84b5ec16619b119cd4468b5e38ceaf3
ThomRojas <thomas.rojas.arce@gmail.com>
2023-02-15 15:06:44 -0300

    Working on client/src/redux reducer.js

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,51 @@
+import {
+  CREATE_VG_ERROR,
+  CREATE_VG_SUCCESS,
+  GET_ALL_VG_ERROR,
+  GET_GENRES_ERROR,
+  GET_GENRES,
+  GET_VG_DETAILS,
+  GET_VG_NAME,
+  GET_ALL_VG,
+} from "./actions";
+
+const initialState = {
+  videoGames: [],
+  allVideoGames: [],
+  genres: [],
+  detail: null,
+};
+
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_ALL_VG:
+      return {
+        ...state,
+        videoGames: action.payload,
+        allVideoGames: action.payload,
+      };
+
+    case GET_VG_NAME:
+      return {
+        ...state,
+        videoGames: action.payload,
+        allVideoGames: action.payload,
+      };
+
+    case GET_VG_DETAILS:
+      return {
+        ...state,
+        detail: action.payload,
+      };
+    case GET_GENRES:
+      return {
+        ...state,
+        genres: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+
+export default rootReducer;
