b789a9a146e64b58e25a0c46dcbae0891a112f0a
ThomRojas <thomas.rojas.arce@gmail.com>
2023-02-16 19:09:30 -0300

    working on landingpage
    working on landingpage

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-
-export default function Loading(){
-    return(
-        <div className="landingbody">
-            <div className="landing">
-            
-                <h1>VIDEOGAMES</h1>
-                
-                <Link className="a" to="/home">
-                    <button className="btn">START</button>
-                </Link>
-
-            </div>
-        </div>
-        
-        
-    )
+import styles from "./LandingPage.module.css";
+const image = "https://i.postimg.cc/T1dW7sG1/Video-Games.png"
+export default function Loading() {
+  return (
+    <div>
+      <div className={styles.landingPage}>
+        <img src={image} alt="Descripción de la imagen"></img>
+      </div>
+      <div>
+        <Link to="/home">
+          <button className={styles.gamerButton}>START </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
